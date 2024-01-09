8e0ccb930b997828a2f75cd459b21ccd321f8482
wuxupeng <wuxupeng@360.cn>
2024-01-09 18:22:57 +0800

    Modify logrotate config

--- logrotate_config/crontab.sh
+++ logrotate_config/crontab.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 echo "[`date "+%Y/%m/%d %H:%M:%S"`] logrotate-start"
-test -x /usr/sbin/logrotate && /usr/sbin/logrotate -f /etc/logrotate.conf
+test -x /usr/sbin/logrotate && /usr/sbin/logrotate /etc/logrotate.conf
 echo "[`date "+%Y/%m/%d %H:%M:%S"`] logrotate-end"
